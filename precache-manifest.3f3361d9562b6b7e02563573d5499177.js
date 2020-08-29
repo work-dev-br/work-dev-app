b575dafc21c100e6363fcb1b4f31445007511aa0
work-dev-br <workdevbr@gmail.com>
2020-08-29 11:12:35 -0300

    Updates

---
+++ precache-manifest.3f3361d9562b6b7e02563573d5499177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff7536b9a86fc915d680226cd04da4a",
+    "url": "/work-dev-app/index.html"
+  },
+  {
+    "revision": "e30eaca0c8a522e006f8",
+    "url": "/work-dev-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5491a5a6b784b4deeb8f",
+    "url": "/work-dev-app/static/js/2.02133858.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/work-dev-app/static/js/2.02133858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30eaca0c8a522e006f8",
+    "url": "/work-dev-app/static/js/main.40161cae.chunk.js"
+  },
+  {
+    "revision": "06a7f4526796b1fa7742",
+    "url": "/work-dev-app/static/js/runtime-main.0a33e7a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/work-dev-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
