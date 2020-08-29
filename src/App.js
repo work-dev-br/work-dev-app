522a6a404cced61cf38fbf46a81f4839304a62bf
work-dev-br <workdevbr@gmail.com>
2020-08-29 11:50:35 -0300

    develop 3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
           Site <code>work.dev.br</code> em construção by nivaldohydalgo.
         </p>
         <p>Branch develop</p>
+        <p>Work in develop branch</p>
     </div>
   );
 }
